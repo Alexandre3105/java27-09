33fc68dc5c92f73787bf05d6cee378c489fec03a
Alexandre Lopes <77018946+Alexandre3105@users.noreply.github.com>
2023-09-27 10:28:53 -0300

    Add files via upload

---
+++ JavaApplication1.java
@@ -0,0 +1,27 @@
+
+package javaapplication1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class JavaApplication1 {
+
+    
+    public static void main(String[] args) {
+        int idade;
+        int lista[];
+        
+        List<String> listaNomes = new ArrayList<>();
+        
+                
+         listaNomes.add("Alexandre");
+         listaNomes.add("Blank");
+         listaNomes.add("POO");
+         
+         System.out.println(listaNomes.size());
+        
+        
+    }
+    
+}
